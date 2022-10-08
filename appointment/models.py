029ee8673196c0b36e61ab9019a71fa9f93aa1f4
Krishan <kslg257@gmail.com>
2022-10-08 09:36:34 +0000

    Remove date accepted from appointment model

--- appointment/models.py
+++ appointment/models.py
@@ -29,11 +29,8 @@ class Appointment(models.Model):
     date = models.DateField()
     time = models.CharField(max_length=80, choices=TIME_SLOTS, default='0')
     email = models.EmailField(max_length=50)
-    phone = models.CharField(max_length=50)
     comments = models.TextField(blank=True)
-    sent_date = models.DateField(auto_now_add=True)
     accepted = models.BooleanField(default=False)
-    accepted_date = models.DateField(auto_now_add=False, null=True, blank=True)
 
     """Good practice to add a magic method that returns a string
        representation of an object.
@@ -41,6 +38,3 @@ class Appointment(models.Model):
     def __str__(self):
         return f"Appointment for {self.child_name} in {self.class_name}\
             with {self.teacher}"
-
-    class Meta:
-        ordering = ["-sent_date"]
