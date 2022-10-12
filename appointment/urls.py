a7ac135b6c96fe2d80828db2c60dae02e3440448
Krishan <kslg257@gmail.com>
2022-10-12 15:25:46 +0000

    Revert indent changes

--- appointment/urls.py
+++ appointment/urls.py
@@ -3,17 +3,16 @@ from django.urls import path
 from .views import appointment_template_view, HomeTemplateView,\
                     ManageAppointmentTemplateView
 from django.contrib.auth.decorators import login_required
+from django.urls import path, include
 
 
 urlpatterns = [
-    path("", HomeTemplateView.as_view(), name="home"),
-    path("make-an-appointment/", views.appointment_template_view,
-         name="appointment"),
-    path("manage-appointments/", login_required(
-         views.ManageAppointmentTemplateView.as_view()), name="manage"),
-    path("login/", views.login_request, name="login"),
-    path("register", views.register_request, name="register"),
-    path('logout/', views.logout_view, name='logout'),
-    path('delete/<int:appointment_id>', views.delete_appointment,
-         name='delete'),
+     path("", HomeTemplateView.as_view(), name="home"),
+     path("make-an-appointment/", views.appointment_template_view, name="appointment"),
+     path("manage-appointments/", login_required(views.ManageAppointmentTemplateView.as_view()), name="manage"),
+     path("login/", views.login_request, name="login"),
+     path("register", views.register_request, name="register"),
+     path('logout/', views.logout_view, name='logout'),
+     path('delete/<int:appointment_id>', views.delete_appointment,
+          name='delete'),
 ]
