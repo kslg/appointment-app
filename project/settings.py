7d7b2246ed92e632a272a5a1f12dbd6350c16184
Krishan <kslg257@gmail.com>
2024-04-26 12:42:27 +0000

    updated settings.py and updated image path for hompage background in CSS.

--- project/settings.py
+++ project/settings.py
@@ -28,11 +28,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ["kslg-appointment-app.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["kslg-appointment-app.herokuapp.com", "localhost", "8000-kslg-appointmentapp-3ge0hca9yda.ws-eu110.gitpod.io"]
 
 # Application definition
 
